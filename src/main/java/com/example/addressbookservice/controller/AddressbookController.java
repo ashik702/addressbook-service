80ea4730c927a6818590b3bc6ddadecac0df5b70
Ashik Ashokkumar <ashik ashokkumar@gmail.com>
2021-08-04 00:06:52 +0530

    Enabled Swagger to Address Book Service

--- src/main/java/com/example/addressbookservice/controller/AddressbookController.java
+++ src/main/java/com/example/addressbookservice/controller/AddressbookController.java
@@ -3,6 +3,7 @@ package com.example.addressbookservice.controller;
 import com.example.addressbookservice.entity.AddressBook;
 import com.example.addressbookservice.service.AddressbookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,17 +20,19 @@ public class AddressbookController {
     private AddressbookService addressbookService;
 
 
+    @ApiOperation(value = "Save single entry to database",response = AddressBook.class)
     @PostMapping("/save")
     public AddressBook saveAddress(@RequestBody AddressBook addressBook){
         return addressbookService.saveAddress(addressBook);
     }
 
-
+    @ApiOperation(value = "Save multiple single entry to database",response = AddressBook.class)
     @PostMapping("/saveAll")
     public List<AddressBook> saveAllAddress(@RequestBody List<AddressBook> addressBookList){
         return addressbookService.saveAlladdress(addressBookList);
     }
     //Search based on any field in AddressBook entity
+    @ApiOperation(value = "Fetch multiple entries based on parameter passed like addressbook/getAddress?lastName=smith",response = AddressBook.class)
     @GetMapping("/getAddress")
     public List<AddressBook> getAddressBookList(
             @RequestParam(value = "firstName", required = false) String firstName,
@@ -44,6 +47,7 @@ public class AddressbookController {
 
     }
 
+    @ApiOperation(value = "Fetch single entry using Id ",response = AddressBook.class)
     @GetMapping("/{id}")
     public AddressBook findById(@PathVariable("id") long addressbookId){
         return addressbookService.findById(addressbookId) ;
